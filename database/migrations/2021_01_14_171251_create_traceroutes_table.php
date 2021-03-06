51a0bf73ac214f4552651bfba5310a0175daa95a
Ryan Partington <pinescore@outlook.com>
2021-03-06 09:32:16 +0000

    added s to table name, sure that's the way we supposed to roll

--- database/migrations/2021_01_14_171251_create_traceroutes_table.php
+++ database/migrations/2021_01_14_171251_create_traceroutes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTracerouteTable extends Migration
+class CreateTraceroutesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTracerouteTable extends Migration
      */
     public function up()
     {
-        Schema::create('traceroute', function (Blueprint $table) {
+        Schema::create('traceroutes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('node');
@@ -28,6 +28,6 @@ class CreateTracerouteTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('traceroute');
+        Schema::dropIfExists('traceroutes');
     }
 }
