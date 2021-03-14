69035eddc224053327ade6d574bafe6a5b225ca5
Ryan Partington <pinescore@outlook.com>
2021-03-14 10:03:39 +0000

    let's run the queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('command:PingCommand')->everyMinute();
         $schedule->command('logs:clear')->daily();
+        $schedule->call('App\Http\Controllers\traceRoute@go')->everyHour();
     }
 
     /**
