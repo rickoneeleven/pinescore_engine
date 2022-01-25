dd9bdab91962bf9b2a1a29445cada62e5877d07e
rickoneeleven <pinescore@outlook.com>
2022-01-25 21:45:28 +0000

    done with this for now, moving onto command intergration

--- app/Http/Controllers/partytest.php
+++ app/Http/Controllers/partytest.php
@@ -4,38 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\group_longterm_scores;
+use App\group_monthly_scores;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class partytest extends Controller
 {
-	//need to create a model for the group_longterm_scores WITHOUT migration to grab the data from the (codeigniter/pinescore original) 
-	//table
-	
-	//$flights = Flight::where('active', 1)
-	//->orderBy('name')
-	//->take(10)
-	//->get();
 	public function index() {
-		echo "hellos";
-		
-		$group_longterm_scores = DB::table('group_longterm_scores')->get();
-		
-		$revenueMonth = group_longterm_scores::where(
-			'datetime', '>=', Carbon::now()->subMonth()->toDateTimeString()
-		);
-		dd($revenueMonth);
-		
-		foreach ($revenueMonth as $revenueMonth) {
-			echo $group_longterm_score->score." willy";
-		}
+		$group_longterm_scores = DB::table('group_longterm_scores')
+				->whereDate('datetime', '>', Carbon::now()->subMonth(1)->toDateTimeString())
+                ->get();
 
 		foreach ($group_longterm_scores as $group_longterm_score) {
-			//echo $group_longterm_score->score;
+			if(!isset($group[$group_longterm_score->group_id])) {
+				$group[$group_longterm_score->group_id] = array();
+			}
+			array_push($group[$group_longterm_score->group_id], $group_longterm_score->score);
+			sort($group[$group_longterm_score->group_id]);
 		}
-		
-
-		
-		
+		foreach($group as $group_id => $scores) {
+			$mean = round(array_sum($scores)/count($scores));
+			echo "group: $group_id<br>scores: ". implode(', ', $scores)."<br>mean: $mean<br><br>";
+			
+			$group_monthly_scores = new group_monthly_scores;
+			$group_monthly_scores->group_id = $group_id;
+			$group_monthly_scores->score = $mean;
+			$group_monthly_scores->save();
+		}
 	}
 }
